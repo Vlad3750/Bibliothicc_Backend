e71157e525b29864a94a9aa5922ba719c08cc6f1
Vlad3750 <vladik.neicovcen@gmail.com>
2026-05-28 14:11:18 +0200

    started with models.py

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = 'sqlite'
+DATABASE_URL = 'sqlite:///database.db'
 
 engine = create_engine(DATABASE_URL)
 
--- models.py
+++ models.py
@@ -1,7 +1,9 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from database import Base
 
 class DBUser(Base):
     __tablename__ = 'user'
 
-    
+    userID = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    isAdmin = Column(Boolean, index=True)
