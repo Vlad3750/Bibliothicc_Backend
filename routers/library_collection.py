7ceabdaca9fe03e527a6185841829adaf95fa8e7
Vlad3750 <vladik.neicovcen@gmail.com>
2026-06-11 11:08:34 +0200

    finished library_collection and updated user

--- routers/library_collection.py
+++ routers/library_collection.py
@@ -10,14 +10,34 @@ from sqlalchemy.orm import Session
 
 router = APIRouter(prefix="/libraryCollection", tags=["libraryCollection"])
 
-class LibraryCollection(BaseModel):
+class LibraryCollectionCreate(BaseModel):
     name: str = Field(..., min_length=3, max_length=30)
+
+class LibraryCollectionResponse(LibraryCollectionCreate):
+    id: int
 
 @cbv(router)
 class LibraryCollectionAPI(BaseAPI):
     db: Session = Depends(get_db)
 
-    @router.get("/", response_model=list[LibraryCollection])
+    @router.get("/", response_model=list[LibraryCollectionResponse])
     def get_all_libraryCollection(self):
         return self.db.query(models.DBLibraryCollection).all()
 
+    @router.get("/{libraryCollection_id}", response_model=LibraryCollectionResponse)
+    def get_libraryCollection_by_id(self, libraryCollection_id: int):
+        return self.get_or_404(self.db, models.DBLibraryCollection, libraryCollection_id)
+
+    @router.post("/")
+    def create_libraryCollection(self, libraryCollection: LibraryCollectionCreate):
+        db_libraryCollection = models.DBLibraryCollection(name=libraryCollection.name)
+        self.db.add(db_libraryCollection)
+        self.db.commit()
+        self.db.refresh(db_libraryCollection)
+        return db_libraryCollection
+
+    @router.delete("/{libraryCollection_id}")
+    def delete_libraryCollection(self, libraryCollection_id: int):
+        db_libraryCollection = self.get_or_404(self.db, models.DBLibraryCollection, libraryCollection_id)
+        self.db.delete(db_libraryCollection)
+        self.db.commit()
